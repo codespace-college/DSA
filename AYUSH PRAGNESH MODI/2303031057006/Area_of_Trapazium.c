03036230619ae845e167c1c869f388fd9dc1813e
codespace-college <145272503+codespace-college@users.noreply.github.com>
2023-11-30 08:01:22 +0000

    main

--- AYUSH PRAGNESH MODI/2303031057006/Area_of_Trapazium.c
+++ AYUSH PRAGNESH MODI/2303031057006/Area_of_Trapazium.c
@@ -3,7 +3,14 @@ float traparea(float a,float b,float h){
     return 0.5*(a+b)*h;
 }
 float main(){
-    float a = 9.0, b=10.0, h=8.0,area;
+    float a, b, h,area;
+    printf("Enter the length of the trapazium: ");
+    scanf("%f",&a);
+    printf("Enter the breadth of the trapazium: ");
+    scanf("%f",&b);
+    printf("Enter the height of the trapazium: ");
+    scanf("%f",&h);
+    
     area=traparea(a,b,h);
     printf("Area of trapazium is : %f \n",area);
     return 0; 
